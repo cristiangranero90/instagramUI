8ef1293e54516f138efdc12348db4daf4f52cbaa
Cristian Gabriel <cristian_granero90@yahoo.com>
2022-11-17 01:47:03 -0300

    Scaffold state added

--- app/src/main/java/com/progressapp/instagramcompose/feeds/FeedScreen.kt
+++ app/src/main/java/com/progressapp/instagramcompose/feeds/FeedScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,8 +68,11 @@ fun FeedScreen(
     for (i in 0 until 10 ){
         users.add(i, user)
     }
+    val scaffoldState = rememberScaffoldState()
 
     Scaffold(
+        scaffoldState = scaffoldState,
+
         topBar = {
             FeedHeader(
             plusClick = { /*TODO*/ },
